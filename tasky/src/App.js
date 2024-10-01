d0b28d07bf1f71e0576366fc90d9770f82e95d60
evan <evanbonner99@gmail.com>
2024-10-01 11:44:40 +0100

    Mask tasks as done

--- tasky/src/App.js
+++ tasky/src/App.js
@@ -10,26 +10,37 @@ function App() {
         title: "Dishes",
         description: "Empty Dishwasher",
         deadline: "Today",
+        done: false,
       },
       {
         id: 2,
         title: "Laundry",
         description: "Fold and put away",
         deadline: "Tomorrow",
+        done: false,
       },
-      { id: 3, title: "Walk Dog", deadline: "Today" },
+      { id: 3, title: "Walk Dog", deadline: "Today", done: false },
     ],
   });
+
+  const doneHandler = (taskIndex) => {
+    const tasks = [...taskState.tasks];
+    tasks[taskIndex].done = !tasks[taskIndex].done;
+    setTaskState({ tasks });
+    console.log(`${taskIndex} ${tasks[taskIndex].done}`);
+  };
 
   return (
     <div className="container">
       <h1>Tasky</h1>
-      {taskState.tasks.map((task) => (
+      {taskState.tasks.map((task, index) => (
         <Task
           title={task.title}
           description={task.description}
           deadline={task.deadline}
           key={task.id}
+          done={task.done}
+          markDone={() => doneHandler(index)}
         />
       ))}
     </div>
