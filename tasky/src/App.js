978feae2ee574ebfd74512a14737a60506980a53
evan <evanbonner99@gmail.com>
2024-10-01 12:06:36 +0100

    Implement adding tasks

--- tasky/src/App.js
+++ tasky/src/App.js
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import { v4 as uuidv4 } from "uuid";
 import Task from "./components/Task";
 import AddTaskForm from "./components/Form";
 import "./App.css";
@@ -24,6 +25,12 @@ function App() {
     ],
   });
 
+  const [formState, setFormState] = useState({
+    title: "",
+    description: "",
+    deadline: "",
+  });
+
   const doneHandler = (taskIndex) => {
     const tasks = [...taskState.tasks];
     tasks[taskIndex].done = !tasks[taskIndex].done;
@@ -34,6 +41,38 @@ function App() {
   const deleteHandler = (taskIndex) => {
     const tasks = [...taskState.tasks];
     tasks.splice(taskIndex, 1);
+    setTaskState({ tasks });
+  };
+
+  const formChangeHandler = (event) => {
+    let form = { ...formState };
+    switch (event.target.name) {
+      case "title":
+        form.title = event.target.value;
+        break;
+      case "description":
+        form.description = event.target.value;
+        break;
+      case "deadline":
+        form.deadline = event.target.value;
+        break;
+      default:
+        form = formState;
+    }
+    setFormState(form);
+  };
+
+  console.log(formState);
+
+  const formSubmitHandler = (event) => {
+    event.preventDefault();
+
+    const tasks = [...taskState.tasks];
+    const form = { ...formState };
+
+    form.id = uuidv4();
+
+    tasks.push(form);
     setTaskState({ tasks });
   };
 
@@ -51,7 +90,7 @@ function App() {
           deleteTask={() => deleteHandler(index)}
         />
       ))}
-      <AddTaskForm />
+      <AddTaskForm submit={formSubmitHandler} change={formChangeHandler} />
     </div>
   );
 }
--- tasky/src/components/Form.js
+++ tasky/src/components/Form.js
@@ -3,23 +3,23 @@ import React from "react";
 function AddTaskForm(props) {
   return (
     <div>
-      <form>
+      <form onSubmit={props.submit}>
         <label>
           Task title:
           <br />
-          <input type="text" name="title" required />
+          <input type="text" name="title" required onChange={props.change} />
         </label>
         <br />
         <label>
           Due date:
           <br />
-          <input type="date" name="deadline" required />
+          <input type="date" name="deadline" required onChange={props.change} />
         </label>
         <br />
         <label>
           Details:
           <br />
-          <input type="text" name="description" />
+          <input type="text" name="description" onChange={props.change} />
         </label>
         <input type="submit" value="Submit" />
       </form>
